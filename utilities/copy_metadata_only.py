2e240de8f5fa1d5ecbc6715c9006a79c2353f97b
Mark Baggett <mbagget1@utk.edu>
2022-10-13 08:33:37 -0400

    Add argument parsing for metadata copy.

--- utilities/copy_metadata_only.py
+++ utilities/copy_metadata_only.py
@@ -1,4 +1,5 @@
 import os
+from argparse import ArgumentParser
 
 
 def copy_metadata_files(path, filename_pattern, output):
@@ -12,4 +13,28 @@ def copy_metadata_files(path, filename_pattern, output):
 
 
 if __name__ == "__main__":
-    copy_metadata_files('/home/mark/PycharmProjects/crossref_journal_minting/', 'conf.xml', 'output')
+    parser = ArgumentParser(description="Copy metadata files from path recursively.")
+    parser.add_argument(
+        "-p",
+        "--path_to_files",
+        dest="path",
+        help="Specify the path where you want to recursively copy files",
+        required=True,
+    )
+    parser.add_argument(
+        "-f",
+        "--filename_pattern",
+        dest="filename_pattern",
+        help="Specify the name of the file you are looking for.",
+        default='metadata.xml'
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        dest="output",
+        help="Specify where you want to write files.",
+        required=True
+    )
+    args = parser.parse_args()
+    #copy_metadata_files('/home/mark/PycharmProjects/crossref_journal_minting/', 'conf.xml', 'output')
+    copy_metadata_files(args.path, args.filename_pattern, args.output)
